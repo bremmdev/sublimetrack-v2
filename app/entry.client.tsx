af32715fc1a78738debf1bfb84a7794e1a07d42b
Matt Bremmers <bremmdev@gmail.com>
2022-08-25 16:38:23 +0200

    re-add hydrateroot

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,4 @@
 import { RemixBrowser } from "@remix-run/react";
-// import { hydrateRoot } from "react-dom/client";
+import { hydrateRoot } from "react-dom/client";
 
-// hydrateRoot(document, <RemixBrowser />);
-
-require("react-dom").hydrate(<RemixBrowser />, document);
+hydrateRoot(document, <RemixBrowser />);
