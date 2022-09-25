26a7e03f714502f7c1d4b6780c8ddee528993e36
Matt Bremmers <bremmdev@gmail.com>
2022-09-25 19:11:10 +0200

    fix themetoggle position on mobile

--- app/components/UI/ThemeToggle.tsx
+++ app/components/UI/ThemeToggle.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { FiSun, FiMoon } from "react-icons/fi";
 
 type Props = {
-  setTheme: (v:string) => void
-}
+  setTheme: (v: string) => void;
+};
 
-const ThemeToggle = (props:Props) => {
+const ThemeToggle = (props: Props) => {
   return (
     <div className="theme-toggle flex">
-      <FiSun size="1.5rem" className="theme-icon" onClick={() => props.setTheme('light')}/>
-      <FiMoon size="1.5rem" className="theme-icon" onClick={() => props.setTheme('dark')}/>
+      <FiSun className="theme-icon" onClick={() => props.setTheme("light")} />
+      <FiMoon className="theme-icon" onClick={() => props.setTheme("dark")} />
     </div>
   );
 };
